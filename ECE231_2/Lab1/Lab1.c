c759fe96eafa9aa2f432195758e3bad2d22d488d
Richard Le <richardle105613@gmail.com>
2025-02-13 10:25:59 -0500

    Creating a file that returns the mean and time stamp

--- ECE231_2/Lab1/Lab1.c
+++ ECE231_2/Lab1/Lab1.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
+#include <time.h>
 
+void mean_and_timestamp(int buffer[],double *mean,struct timespec *timestamp) {
+    double sum = 0.0;
+
+    for (int i = 0; i < 10; i++) {
+        sum += buffer[i];
+    }
+    *mean = sum/10;
+
+    clock_gettime(CLOCK_MONOTONIC,timestamp);
+}
 int main() {
-    int num;
-    int buffer[10]; 
+    double mean; // the mean will be a float
+    int buffer[10]; // an array of 10 integers
+    struct timespec timestamp; // struct stores related variables
 
-    printf("Enter an integer in the range (0-9000) please: ");
+    int count = 0; // initialize the count for inputting 10 ints
+    while (count < 10){
+        int num;
+        printf("Enter an integer(%d/10): ",count + 1);
 
-    if (scanf("%d", &num) != 1) { 
-        printf("Please enter a number between 0 and 9000\n");
-        return 1;
+        if (scanf("%d",&num) != 1) {
+            printf("Invalid input, please input a number (0-9000)\n");
+            continue;
+        }
+
+        if (num < 0 || num > 9000) {
+            printf("Invalid input, please input a number (0-9000)\n");
+            continue;
+        }
+
+        buffer[count++] = num;
     }
 
-    if (num < 0 || num > 9000) { 
-        printf("Please enter a number between 0 and 9000\n");
-        return 1;
-    }
+    mean_and_timestamp(buffer,&mean,&timestamp);
 
-    buffer[0] = num; // Store input into the first index of the buffer array
-
-    printf("Stored %d in buffer[0]\n", buffer[0]);
-    return 0;
+    printf("Results: \n");
+    printf("Mean: %.2f\n", mean);
+    printf("Timestamp: %ld seconds, %ld nanoseconds\n",timestamp.tv_sec,timestamp.tv_nsec);
 }
