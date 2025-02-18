a3dcc0544049def5f7270287043bb92fa852e967
Richard Le <richardle105613@gmail.com>
2025-02-17 22:00:10 -0500

    Finishing Lab1

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <time.h>
 
-void mean_and_timestamp(int buffer[],double *mean,struct timespec *timestamp) {
-    int sum = 0;
+void mean_and_timestamp(int buffer[],double *mean,struct timespec *timestamp) { // timespec is used to store sec and nsec
+    int sum = 0;                            
 
     for (int i = 0; i < 10; i++) {
         sum += buffer[i];
     }
-    *mean = sum/10;
-
-    clock_gettime(CLOCK_MONOTONIC,timestamp);
+    *mean = (sum/10); // pointer to mean, sets mean to sum/10
+    clock_gettime(CLOCK_MONOTONIC,timestamp); // stores a sec and nsec into the timespec struct named timestamp
 }
+
 int main() {
-    double mean; // we can make the mean an int later 
-    int buffer[10]; // an array of 10 integers
-    struct timespec timestamp; // struct stores related variables
+    double mean; 
+    int buffer[10]; 
+    struct timespec timestamp; 
 
     int round = 0; 
     while (round < 2) {
@@ -25,12 +25,6 @@ int main() {
             printf("Enter an integer(%d/10): ",count + 1);
             int x = scanf("%d",&num); 
 
-            if (x != 1) {
-                printf("Invalid input, please input a number (0-9000)\n");
-                while (getchar() != '\n'); // discard the invalid input
-                continue;
-            }
-
             if (num < 0 || num > 9000) {
                 printf("Invalid input, please input a number (0-9000)\n");
                 continue;
@@ -39,10 +33,10 @@ int main() {
             buffer[count++] = num;
         }
         round++;
-        mean_and_timestamp(buffer,&mean,&timestamp);
+        mean_and_timestamp(buffer,&mean,&timestamp); // give memory addresses to custom function
 
         printf("Results of round %d: \n",round);
-        printf("Mean: %.2f\n", mean);
-        printf("Timestamp: %ld seconds, %ld nanoseconds\n",timestamp.tv_sec,timestamp.tv_nsec);
+        printf("Mean: %d\n", (int) mean);
+        printf("Timestamp: %ld seconds, %ld nanoseconds\n",timestamp.tv_sec,timestamp.tv_nsec); //tv_sec and tv+nsec are stored in the timespec structure named timestamp
     }
 }
